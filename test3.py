71c0df89d6d89e57f1287afb455009e9b9cf1337
AmirSolt <soltamir194@gmail.com>
2023-07-10 23:22:26 -0400

    css selector for invisble elements

--- test3.py
+++ test3.py
@@ -1,23 +1,44 @@
 script = """
 
-function getVisibleChildren(parent){
-  let visibleChildren = Array.from(parent.children).filter(el => {
-    let rect = el.getBoundingClientRect();
-    if(!(rect.width>0 && rect.height>0)){return false}
-    let style = window.getComputedStyle(el);
-    if(style.display === 'none'){return false}
-    if(style.visibility === 'hidden'){return false}
-    return true;
-  }).map(el => ({
-    el: el,
-    children: getVisibleChildren(el)
-  }));
-  
-  return visibleChildren;
+function getUniqueCssPath(el) {
+  if (!el || el.nodeType !== Node.ELEMENT_NODE) {
+    return '';
+  }
+
+  const path = [];
+  while (el.nodeType === Node.ELEMENT_NODE && el.nodeName.toLowerCase() !== 'html') {
+    const selector = el.nodeName.toLowerCase() + 
+      (el.id ? `#${el.id}` : '') +
+      (el.className && typeof el.className === 'string' 
+        ? '.' + el.className.trim().replace(/\s+/g, ".") 
+        : '');
+    path.unshift(selector);
+    el = el.parentNode;
+  }
+
+  return path.join(' > ');
 }
 
-let visibleElements = getVisibleChildren(document.body);
-return visibleElements
+function isElementInvisible(el){
+    let rect = el.getBoundingClientRect();
+    if(!(rect.width>0 && rect.height>0)){return true}
+    let style = window.getComputedStyle(el);
+    if(style.display === 'none'){return true}
+    if(style.visibility === 'hidden'){return true}
+    return false;
+}
+
+
+function getInvisibleChildren(parent){
+  let invisibleChildren = Array.from(parent.children).filter(el => {
+      return isElementInvisible(el)
+  })
+  
+  return invisibleChildren;
+}
+let invisibleElements = getInvisibleChildren(document.body);
+let invisibleSelectors = invisibleElements.map(getUniqueCssPath);
+return invisibleSelectors
 
 """
 
@@ -36,4 +57,7 @@ r = driver.execute_script(script)
 
 driver.close()
 
-utils.write_file("test.html", r)
+utils.write_file("test.json", r)
+
+
+
