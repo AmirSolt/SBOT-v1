e828ff9b68c2611b13d7633b7bfb1d2a6d38af53
AmirSolt <soltamir194@gmail.com>
2023-07-19 13:02:48 -0400

    floor fixed

--- helper/scripts/floor.js
+++ helper/scripts/floor.js
@@ -1,22 +1,40 @@
 
-const HIERARCHY_COEFFICIENT = 5;
+const HIERARCHY_COEFFICIENT = 10;
 const AREA_COEFFICIENT = 5;
-const POS_COEFFICIENT = 5;
-const INNERHTML_COEFFICIENT = 5;
+const POS_COEFFICIENT = 10;
+// const INNERHTML_COEFFICIENT = 5;
 
-const AVG_GROUP_BODY_RATIO = 0.2;
-const MIN_GROUP_SCORE = 0.0;
+const MIN_Floor_SCORE = 2.0;
 
 
+function getUniqueCssPath(el) {
+    if (!el || el.nodeType !== Node.ELEMENT_NODE) {
+        return '';
+    }
+    const path = [];
+    while (el.nodeType === Node.ELEMENT_NODE && el.nodeName.toLowerCase() !== 'html') {
+        let selector = el.nodeName.toLowerCase();
+        if (el.parentNode) {
+            const siblings = Array.from(el.parentNode.children);
+            const sameTagSiblings = siblings.filter(sibling => sibling.nodeName.toLowerCase() === selector);
+            if (sameTagSiblings.length > 1) {
+                selector += `:nth-child(${siblings.indexOf(el) + 1})`;
+            }
+        }
+        path.unshift(selector);
+        el = el.parentNode;
+    }
+    return path.join(' > ');
+}
 
 
 function getDistanceRatio(p0, p1, size){
     return Math.abs(p0 - p1)/size
 }
 
-function getRectCenter(rect){
-    const centerX = rect.left + rect.width / 2;
-    const centerY = rect.top + rect.height / 2;
+function getRectCenter(x, y, w, h){
+    const centerX = x + w / 2;
+    const centerY = y + h / 2;
     
     return [centerX, centerY]
 }
@@ -74,13 +92,15 @@ function getHierarchyScore(el, style) {
 
     if (tagName === 'dialog') {
         order = 0.3;
-    } else if (position === 'absolute' ||
-        position === 'relative' ||
+    } 
+    else if (position === 'absolute' ||
+        // position === 'relative' ||
         position === 'fixed') {
         order = 0.2;
-    } else if (style.getPropertyValue('display').startsWith('inline')) {
-        order = 0.1;
-    }
+    } 
+    // else if (style.getPropertyValue('display').startsWith('inline')) {
+    //     order = 0.1;
+    // }
 
     if(parseInt(zIndex, 10) > 0){
         order += 0.05
@@ -91,18 +111,22 @@ function getHierarchyScore(el, style) {
     return order*HIERARCHY_COEFFICIENT;
 }
 
-function getGroupPosScore(rect, bodyRect){
-    const [x, y] = getRectCenter(rect);
-    const [bx, by] = getRectCenter(bodyRect);
+function getFloorPosScore(rect){
+    
+    const [x, y] = getRectCenter(rect.left, rect.top, rect.width, rect.height);
+    const [sx, sy] = getRectCenter(0, 0, window.screen.width, window.screen.height);
 
-    const xScore = getDistanceRatio(x, bx, bodyRect.width);
-    const yScore = getDistanceRatio(y, bodyRect.top+(bodyRect.height*0.2), bodyRect.height);
+    let xScore = getDistanceRatio(x, sx, window.screen.width);
+    let yScore = getDistanceRatio(y, sy, window.screen.height);
 
-    return (xScore*0.8) * (yScore*0.2) * POS_COEFFICIENT
+    xScore = 1-xScore
+    yScore = 1-yScore
+
+    return ((xScore*0.5) + (yScore*0.5)) * POS_COEFFICIENT
 }
-function getGroupAreaScore(rect, bodyRect){
+function getFloorAreaScore(rect, bodyRect){
     const areaRatio = getRectArea(rect)/getRectArea(bodyRect);
-    return (1/Math.abs(AVG_GROUP_BODY_RATIO-areaRatio)) * AREA_COEFFICIENT;
+    return areaRatio * AREA_COEFFICIENT;
 }
 
 function getInnerHtmlScore(el, bodyHtmlSize){
@@ -115,7 +139,7 @@ function avgScore(numbers){
 }
 
 
-function getGroupElementScore(el, bodyInfo){
+function getFloorElementScore(el, bodyInfo){
 
     const rect = el.getBoundingClientRect();
     
@@ -128,17 +152,17 @@ function getGroupElementScore(el, bodyInfo){
     const style = window.getComputedStyle(el);
 
     const scores = [
-        getInnerHtmlScore(el, bodyInfo.bodyHtmlSize),
         getHierarchyScore(el, style),
-        getGroupPosScore(rect, bodyInfo.bodyRect),
-        getGroupAreaScore(rect, bodyInfo.bodyRect),
+        getFloorAreaScore(rect, bodyInfo.bodyRect),
+        getFloorPosScore(rect, bodyInfo.bodyRect),
+        // getInnerHtmlScore(el, bodyInfo.bodyHtmlSize),
     ]
     
     return avgScore(scores)
 }
 
 
-function getGroupElementsByContext(context, contextPath){
+function getFloorElementsByContext(context, contextPath){
     let elements = context.body.querySelectorAll("*");
     let bodyInfo = {
         bodyRect: context.body.getBoundingClientRect(),
@@ -146,53 +170,49 @@ function getGroupElementsByContext(context, contextPath){
     }
 
     
-    const groupInfos = Array.from(elements).map(el=>{
+    const FloorInfos = Array.from(elements).map(el=>{
 
         return {
-            context:context,
             contextPath:contextPath,
             element:el,
-            score:getGroupElementScore(el, bodyInfo)
+            score:getFloorElementScore(el, bodyInfo)
 
         }
     })
-    return groupInfos
+    return FloorInfos
 }
 
 
-function getGroups(){
-    let groups = []
+function getFloors(){
+    let Floors = []
 
-    const bodyGroups = getGroupElementsByContext(document, "")
-    if(bodyGroups!=null)
-        groups.push(...bodyGroups)
+    const bodyFloors = getFloorElementsByContext(document, "")
+    if(bodyFloors!=null)
+        Floors.push(...bodyFloors)
     
     Array.from(getAllIframes(document)).forEach(iframe => {
         try {
             let iframeContent = iframe.contentWindow.document;
             const contextPath = getUniqueCssPath(iframe)
-            const iframeGroups = getGroupElementsByContext(iframeContent, contextPath)
-            if(iframeGroups!=null)
-                groups.push(...iframeGroups)
+            const iframeFloors = getFloorElementsByContext(iframeContent, contextPath)
+            if(iframeFloors!=null)
+                Floors.push(...iframeFloors)
         }catch(e){
             console.log(e)
         }
     });
 
-    return groups
+    return Floors
 }
 
 
-function getOrderedGroupByIndex(groups, orderIndex=0){
-    if(groups.length>orderIndex)
-        return groups.sort((a,b) => b.score - a.score)[orderIndex];
-
-    return null
+function getOrderedFloors(Floors){
+    return Floors.sort((a,b) => b.score - a.score)
 }
 
 function highlightElement(target, color, text){
     console.log(target)
-    console.log(target.style)
+    // console.log(target.style)
     target.style.borderColor = color;
     target.style.borderStyle = 'solid';
     var container = document.createElement('div');
@@ -211,19 +231,27 @@ function highlightElement(target, color, text){
 }
 
 
-function highlightGroup(){
-    const groups = getGroups()
 
-    // console.log(groups)
 
-    const groupInfo = getOrderedGroupByIndex(groups, orderIndex=0)
-    highlightElement(groupInfo["element"], "red", "group 0")
+function highlightFloor(){
+    const Floors = getFloors()
 
-    const groupInfo1 = getOrderedGroupByIndex(groups, orderIndex=1)
-    highlightElement(groupInfo1["element"], "green", "group 1")
+    getOrderedFloors(Floors).slice(0,20).forEach((floor, i)=>{
+        if(floor["score"]>MIN_Floor_SCORE)
+            // highlightElement(floor["element"], "red", `${floor["score"].toFixed(2)}`)
+            highlightElement(floor["element"], "red", `${i}`)
+    })
 
-    const groupInfo2 = getOrderedGroupByIndex(groups, orderIndex=2)
-    highlightElement(groupInfo2["element"], "blue", "group 2")
+
+
+    // const FloorInfo = getOrderedFloorByIndex(Floors, orderIndex=0)
+    // highlightElement(FloorInfo["element"], "red", `${FloorInfo["score"].toFixed(2)}`)
+
+    // const FloorInfo1 = getOrderedFloorByIndex(Floors, orderIndex=1)
+    // highlightElement(FloorInfo1["element"], "green", `${FloorInfo1["score"].toFixed(2)}`)
+
+    // const FloorInfo2 = getOrderedFloorByIndex(Floors, orderIndex=2)
+    // highlightElement(FloorInfo2["element"], "blue", `${FloorInfo2["score"].toFixed(2)}`)
 }
 
-highlightGroup()
+highlightFloor()
