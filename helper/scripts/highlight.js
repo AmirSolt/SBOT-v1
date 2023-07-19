ff2285ed7bebab9498bed738a675b434f92722fe
AmirSolt <soltamir194@gmail.com>
2023-07-19 10:42:36 -0400

    updated vision

--- helper/scripts/highlight.js
+++ helper/scripts/highlight.js
@@ -1,17 +1,20 @@
 
-let target = document.querySelector('#rcnt')
-target.style.borderColor = 'red';
-target.style.borderStyle = 'solid';
-var container = document.createElement('div');
-container.style.position = 'relative'
-var label = document.createElement('div');
 
-label.textContent = 'My Label';
-label.style.position = 'absolute';
-label.style.top = '-40px';
-label.style.left = '0';
-label.style.borderColor = 'red';
-label.style.borderStyle = 'solid';
 
-container.insertBefore(label, container.firstChild)
-target.insertBefore(container, target.firstChild);
+function highlightElement(target, color, label){
+    target.style.borderColor = color;
+    target.style.borderStyle = 'solid';
+    var container = document.createElement('div');
+    container.style.position = 'relative'
+    var label = document.createElement('div');
+    
+    label.textContent = label;
+    label.style.position = 'absolute';
+    label.style.top = '-40px';
+    label.style.left = '0';
+    label.style.borderColor = color;
+    label.style.borderStyle = 'solid';
+    
+    container.insertBefore(label, container.firstChild)
+    target.insertBefore(container, target.firstChild);
+}
--- helper/scripts/vision.js
+++ helper/scripts/vision.js
@@ -1,7 +1,35 @@
+
+const HIERARCHY_COEFFICIENT = 5;
+const AREA_COEFFICIENT = 5;
+const POS_COEFFICIENT = 5;
+const INNERHTML_COEFFICIENT = 5;
+
+const AVG_GROUP_BODY_RATIO = 0.2;
+const MIN_GROUP_SCORE = 0.0;
+
+
+
+
+function getDistanceRatio(p0, p1, size){
+    return Math.abs(p0 - p1)/size
+}
+
+function getRectCenter(rect){
+    const centerX = rect.left + rect.width / 2;
+    const centerY = rect.top + rect.height / 2;
+    
+    return [centerX, centerY]
+}
+
+function distanceBetweenCenters(x0, y0, x1, y1) {
+    const xDiff = x1 - x0;
+    const yDiff = y1 - y0;
+    return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+}
+
 function getAllIframes(document){
     return document.querySelectorAll("iframe")
 }
-
 
 function indexOfMax(arr) {
     if (arr.length === 0) {
@@ -18,6 +46,14 @@ function indexOfMax(arr) {
     return maxIndex;
 }
 
+function hasInnerText(el){
+    if(el.innerText==null)
+        return false
+    if(el.innerText.length>0)
+        return true
+    return false
+}
+
 function isSpatial(el, rect) {
     if (!(rect.width > 0 && rect.height > 0)) { return false }
     let style = window.getComputedStyle(el);
@@ -26,135 +62,166 @@ function isSpatial(el, rect) {
     return true;
 }
 
-function getHierarchyScore(style) {
+function getRectArea(rect){
+    return rect.width * rect.height
+}
+
+function getHierarchyScore(el, style) {
     const position = style.getPropertyValue('position');
     const zIndex = style.getPropertyValue('z-index');
-    const tagName = element.tagName.toLowerCase();
-    let order;
+    const tagName = el.tagName.toLowerCase();
+    let order = 0.0;
 
     if (tagName === 'dialog') {
-        order = 30000;
+        order = 0.3;
     } else if (position === 'absolute' ||
         position === 'relative' ||
         position === 'fixed') {
-        order = parseInt(zIndex, 10) || 0;
-        order += 20000;
+        order = 0.2;
     } else if (style.getPropertyValue('display').startsWith('inline')) {
-        order = 10000;
-    } else {
-        order = parseInt(zIndex, 10) || 0;
+        order = 0.1;
     }
 
-    return order;
-}
+    if(parseInt(zIndex, 10) > 0){
+        order += 0.05
+    }
 
-function getRectCenter(rect){
-    const centerX = rect.left + rect.width / 2;
-    const centerY = rect.top + rect.height / 2;
-    
-    return [centerX, centerY]
-}
+    if(order>1)order=1
 
-function distanceBetweenCenters(x0, y0, x1, y1) {
-    const xDiff = x1 - x0;
-    const yDiff = y1 - y0;
-    return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
-}
-
-function getRectArea(rect){
-    return rect.width * rect.height
+    return order*HIERARCHY_COEFFICIENT;
 }
 
 function getGroupPosScore(rect, bodyRect){
     const [x, y] = getRectCenter(rect);
     const [bx, by] = getRectCenter(bodyRect);
-    return distanceBetweenCenters(x,y,bx,by)
+
+    const xScore = getDistanceRatio(x, bx, bodyRect.width);
+    const yScore = getDistanceRatio(y, bodyRect.top+(bodyRect.height*0.2), bodyRect.height);
+
+    return (xScore*0.8) * (yScore*0.2) * POS_COEFFICIENT
 }
 function getGroupAreaScore(rect, bodyRect){
     const areaRatio = getRectArea(rect)/getRectArea(bodyRect);
-    return 1/Math.abs(AVG_GROUP_BODY_RATIO-areaRatio)
+    return (1/Math.abs(AVG_GROUP_BODY_RATIO-areaRatio)) * AREA_COEFFICIENT;
+}
+
+function getInnerHtmlScore(el, bodyHtmlSize){
+    return (el.innerHTML.length / bodyHtmlSize) * INNERHTML_COEFFICIENT
+}
+
+function avgScore(numbers){
+    const sum = numbers.reduce((a, b) => a + b, 0);
+    return (sum / numbers.length) || 0;
 }
 
 
-
-
-function resultScore(hierarchyScore, posScore, areaScore){
-    return 0
-}
-
-
-function getGroupElementScore(el, bodyRect){
+function getGroupElementScore(el, bodyInfo){
 
     const rect = el.getBoundingClientRect();
     
     if(!isSpatial(el, rect))
         return 0
 
-    // if no all_text return 0
+    if(!hasInnerText(el))
+        return 0
 
     const style = window.getComputedStyle(el);
 
-
-    // get score for innerHTML volume
-    const hierarchyScore = getHierarchyScore(style);
-    const posScore = getGroupPosScore(rect, bodyRect);
-    const areaScore = getGroupAreaScore(rect, bodyRect);
+    const scores = [
+        getInnerHtmlScore(el, bodyInfo.bodyHtmlSize),
+        getHierarchyScore(el, style),
+        getGroupPosScore(rect, bodyInfo.bodyRect),
+        getGroupAreaScore(rect, bodyInfo.bodyRect),
+    ]
     
-    return resultScore(hierarchyScore, posScore, areaScore)
+    return avgScore(scores)
 }
 
 
-function getGroupElementByContext(context){
+function getGroupElementsByContext(context){
     let elements = context.body.querySelectorAll("*");
-    let bodyRect = context.body.getBoundingClientRect();
-
-    
-    const groupScores = Array.from(elements).map(el=>{
-        return getGroupElementScore(el, bodyRect)
-    })
-
-    const biggestInx = indexOfMax(groupScores)
-
-    if(groupScores[biggestInx] <  MIN_GROUP_SCORE)
-        return null
-
-    return {
-        context:context,
-        element: elements[biggestInx],
-        score:groupScores[biggestInx],
+    let bodyInfo = {
+        bodyRect: context.body.getBoundingClientRect(),
+        bodyHtmlSize: context.body.innerHTML.length,
     }
 
+    
+    const groupInfos = Array.from(elements).map(el=>{
+
+        return {
+            context:context,
+            element:el,
+            score:getGroupElementScore(el, bodyInfo)
+
+        }
+    })
+    return groupInfos
 }
 
 
-
-
-
-
-const AVG_GROUP_BODY_RATIO = 0.2;
-const MIN_GROUP_SCORE = 10;
-
-function getGroupElement(){
-
+function getGroups(){
     let groups = []
 
-    const group = getGroupElementByContext(document)
-    if(group!=null)
-        groups.push(group)
+    const bodyGroups = getGroupElementsByContext(document)
+    if(bodyGroups!=null)
+        groups.push(...bodyGroups)
     
     Array.from(getAllIframes(document)).forEach(iframe => {
         try {
             let iframeContent = iframe.contentWindow.document;
-            const iframeGroup = getGroupElementByContext(iframeContent)
-            if(iframeGroup!=null)
-                groups.push(iframeGroup)
+            const iframeGroups = getGroupElementsByContext(iframeContent)
+            if(iframeGroups!=null)
+                groups.push(...iframeGroups)
         }catch(e){
             console.log(e)
         }
     });
 
-    if(groups.length>0)
-        return groups.sort((a,b) => b.score - a.score)[0];
+    return groups
+}
+
+
+function getOrderedGroupByIndex(groups, orderIndex=0){
+    if(groups.length>orderIndex)
+        return groups.sort((a,b) => b.score - a.score)[orderIndex];
 
     return null
 }
+
+function highlightElement(target, color, text){
+    console.log(target)
+    console.log(target.style)
+    target.style.borderColor = color;
+    target.style.borderStyle = 'solid';
+    var container = document.createElement('div');
+    container.style.position = 'relative'
+    var label = document.createElement('div');
+    
+    label.textContent = text;
+    label.style.position = 'absolute';
+    label.style.top = '-40px';
+    label.style.left = '0';
+    label.style.borderColor = color;
+    label.style.borderStyle = 'solid';
+    
+    container.insertBefore(label, container.firstChild)
+    target.insertBefore(container, target.firstChild);
+}
+
+
+function highlightGroup(){
+    const groups = getGroups()
+
+    // console.log(groups)
+
+    const groupInfo = getOrderedGroupByIndex(groups, orderIndex=0)
+    highlightElement(groupInfo["element"], "red", "group 0")
+
+    const groupInfo1 = getOrderedGroupByIndex(groups, orderIndex=1)
+    highlightElement(groupInfo1["element"], "green", "group 1")
+
+    const groupInfo2 = getOrderedGroupByIndex(groups, orderIndex=2)
+    highlightElement(groupInfo2["element"], "blue", "group 2")
+}
+
+highlightGroup()
