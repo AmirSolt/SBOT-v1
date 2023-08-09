5133e307c03250add71478b25442b69b6d2e8812
AmirSolt <soltamir194@gmail.com>
2023-08-08 20:43:35 -0400

    upd

---
+++ test_pages/branches.js
@@ -0,0 +1,365 @@
+function getPageRect(doc){
+    const zBody = doc.body,
+    zHtml = document.documentElement;
+    const zWidth = Math.max( zBody.scrollWidth, zBody.offsetWidth, 
+        zHtml.clientWidth, zHtml.scrollWidth, zHtml.offsetWidth );
+    const zHeight = Math.max( zBody.scrollHeight, zBody.offsetHeight, 
+                        zHtml.clientHeight, zHtml.scrollHeight, zHtml.offsetHeight );
+    return new Rect(0, 0, zWidth, zHeight)
+}
+function getUniqueCssPath(el) {
+    if (!el || el.nodeType !== Node.ELEMENT_NODE) {
+        return '';
+    }
+    const path = [];
+    while (el.nodeType === Node.ELEMENT_NODE && el.nodeName.toLowerCase() !== 'html') {
+        let selector = el.nodeName.toLowerCase();
+        if (el.parentNode) {
+            const siblings = Array.from(el.parentNode.children);
+            const sameTagSiblings = siblings.filter(sibling => sibling.nodeName.toLowerCase() === selector);
+            if (sameTagSiblings.length > 1) {
+                selector += `:nth-child(${siblings.indexOf(el) + 1})`;
+            }
+        }
+        path.unshift(selector);
+        el = el.parentNode;
+    }
+    return path.join(' > ');
+}
+function cleanText(text) {
+    return text.replace(/\s+/g, ' ').trim();
+}
+function getAllText(element) {
+    const text = element.innerText || '';
+    return cleanText(text)
+}
+function getDirectText(el) {
+    let directText = '';
+    for (let i = 0; i < el.childNodes.length; i++) {
+        if (el.childNodes[i].nodeType === 3) {
+            directText += el.childNodes[i].nodeValue;
+        }
+    }
+    return cleanText(directText)
+}
+function isValidHttpUrl(string) {
+    if (string[0] == "/")
+        return true
+
+    let url;
+    try {
+        url = new URL(string);
+    } catch (_) {
+        return false;
+    }
+
+    return url.protocol === "http:" || url.protocol === "https:"
+}
+function highlight(rect, color, label, id) {
+    const svgNS = 'http://www.w3.org/2000/svg';
+
+    // var idElements = document.querySelectorAll(`#${id}`);
+    // Array.from(idElements).forEach(function(element) {
+    //     element.remove();
+    // });
+
+    // Create SVG element
+    const svg = document.createElementNS(svgNS, 'svg');
+    svg.setAttribute("id", id)
+    svg.style.position = 'fixed';
+    svg.style.top = '0';
+    svg.style.left = '0';
+    svg.style.width = '100%';
+    svg.style.height = '100%';
+    svg.style.pointerEvents = 'none';
+    svg.style.zIndex = '9999';
+    svg.style.fontSize = unit;
+
+    // Create Rectangle
+    const rectangle = document.createElementNS(svgNS, 'rect');
+    rectangle.setAttribute('x', rect.x);
+    rectangle.setAttribute('y', rect.y);
+    rectangle.setAttribute('width', rect.w);
+    rectangle.setAttribute('height', rect.h);
+    rectangle.setAttribute('stroke-width', unit / 8);
+    rectangle.setAttribute('stroke', color);
+    rectangle.setAttribute('fill', 'none');
+
+    // Create Label
+    const text = document.createElementNS(svgNS, 'text');
+    text.setAttribute('x', rect.x);
+    text.setAttribute('y', Number(rect.y) + Number(rect.h)); // Position below the rectangle
+    text.setAttribute('dy', '1.2em'); // Offset below the rectangle
+    text.setAttribute('font-size', unit);
+    text.setAttribute('fill', color); // Color of text same as box's
+    text.setAttribute('stroke', "none");
+    text.textContent = label;
+
+    // Add elements to SVG and then to Body
+    svg.appendChild(rectangle);
+    svg.appendChild(text);
+    document.body.appendChild(svg);
+
+}
+
+class Rect {
+    AlignIds = {
+        x0 : "left",
+        x1 : "center_x",
+        x2 : "right",
+        y0 : "top",
+        y1 : "center_y",
+        y2 : "bottom",
+    }
+
+    constructor(x, y, w, h) {
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.cx = this.x + this.w / 2;
+        this.cy = this.y + this.h / 2;
+    }
+    get area() {
+        return this.w * this.h
+    }
+    getAlignment(alignId){
+        switch (alignId.toLowerCase().trim()) {
+            case Rect.AlignIds.x0:
+                return this.x
+            case Rect.AlignIds.x1:
+                return this.cx
+            case Rect.AlignIds.x2:
+                return this.x+this.w
+            case Rect.AlignIds.y0:
+                return this.y
+            case Rect.AlignIds.y1:
+                return this.cy
+            case Rect.AlignIds.y2:
+                return this.y+this.h
+            default:
+                throw new Error(`direction: ${direction} doesnt exist`);
+        }
+    }
+    static elementToRect(element) {
+        const domRect = element.getBoundingClientRect()
+        return new Rect(domRect.left + window.scrollX, domRect.top + window.scrollY, domRect.width, domRect.height)
+        // return new Rect(domRect.left, domRect.top, domRect.width, domRect.height)
+    }
+    static isWithinMargin(rect0, rect1, distance, directions) {
+        const directionsArr = directions.split(",")
+        const conditions = directionsArr.map(direction => {
+            switch (direction.toLowerCase().trim()) {
+                case "l":
+                    return Rect.isWithinXMargin(rect0, rect1, -distance) &&
+                        Rect.areYBoundsOverlaping(rect0, rect1)
+                case "r":
+                    return Rect.isWithinXMargin(rect0, rect1, distance) &&
+                        Rect.areYBoundsOverlaping(rect0, rect1)
+                case "t":
+                    return Rect.isWithinYMargin(rect0, rect1, -distance) &&
+                        Rect.areXBoundsOverlaping(rect0, rect1)
+                case "b":
+                    return Rect.isWithinYMargin(rect0, rect1, distance) &&
+                        Rect.areXBoundsOverlaping(rect0, rect1)
+                default:
+                    throw new Error(`direction: ${direction} doesnt exist`);
+            }
+        })
+
+        return conditions.some(v => v == true)
+    }
+    static areXBoundsOverlaping(rect0, rect1) {
+        return (
+            (rect0.x < rect1.x + rect1.w) &&
+            (rect0.x + rect0.w > rect1.x)
+        )
+    }
+    static areYBoundsOverlaping(rect0, rect1) {
+        return (rect0.y < rect1.y + rect1.h) &&
+            (rect0.y + rect0.h > rect1.y)
+    }
+    static isWithinXMargin(rect0, rect1, distance) {
+        // <0 is left
+        let d = rect1.cx - rect0.cx
+        const direction = (d / Math.abs(d))
+        let margin = d - ((rect0.w / 2 + rect1.w / 2) * direction)
+        return (margin * direction) < (distance * direction)
+    }
+    static isWithinYMargin(rect0, rect1, distance) {
+        // <0 is top
+        let d = rect1.cy - rect0.cy
+        const direction = (d / Math.abs(d))
+        let margin = d - ((rect0.h / 2 + rect1.h / 2) * direction)
+        return (margin * direction) < (distance * direction)
+    }
+    static combineRects(rects) {
+        var minX = Infinity, minY = Infinity, maxX = -Infinity, maxY = -Infinity;
+
+        rects.forEach(function (rect) {
+            if (rect == null)
+                return null
+
+            minX = Math.min(minX, rect.x);
+            minY = Math.min(minY, rect.y);
+            maxX = Math.max(maxX, rect.x + rect.w);
+            maxY = Math.max(maxY, rect.y + rect.h);
+        });
+
+        return new Rect(minX, minY, maxX - minX, maxY - minY);
+    }
+    static isContaining(parentRect, childRect) {
+        return (
+            (parentRect.x <= childRect.x) &&
+            (parentRect.y <= childRect.y) &&
+            (parentRect.y + parentRect.h >= childRect.y) &&
+            (parentRect.x + parentRect.w >= childRect.x)
+        )
+    }
+}
+class Vis {
+    static isSpatial(style, rect) {
+        if (!(rect.w > 0 && rect.h > 0)) { return false }
+        if (style.display === 'none') { return false }
+        if (style.visibility === 'hidden') { return false }
+        return true;
+    }
+    static isLowVis(style) {
+        if (style.filter !== "none" || parseInt(style.opacity) < 1) {
+            return true;
+        }
+        return false;
+    }
+    static hasInnerText(el) {
+        const innerText = el.innerText
+        if (innerText == null)
+            return false
+        if (innerText.length > 0)
+            return true
+        return false
+    }
+    static hasBG(style) {
+        return !(style.backgroundColor === 'rgba(0, 0, 0, 0)' ||
+            style.backgroundColor === 'transparent')
+    }
+}
+
+// ================================
+
+class Leaf{
+    constructor(element){
+    }
+    static isToAvoide(element){
+        // is link
+    }
+    static isType(element){
+    }
+    static getLeaf(element){
+        const LeafClass = LeafClasses.find(LeafClass => LeafClass.isType(element))
+        if (!LeafClass)
+            return null
+        return new LeafClass(el);
+    }
+}
+// ======
+class LInteractive extends Leaf{
+}
+class LInput extends LInteractive{
+}
+class LSelect extends LInteractive{
+}
+class LPointer extends LInteractive{
+    // has pointer and pointer event
+}
+// ======
+class LString extends Leaf{
+}
+class LText extends LString{
+}
+class LOption extends LString{
+    // if atleast one sibling identical
+}
+// ======
+class LMedia extends Leaf{
+}
+class LImage extends LMedia{
+}
+
+function getLeafs(doc){
+    function traverseChildren(element, leafs) {
+        if (Leaf.toAvoid(element)) {
+            return leafs
+        }
+
+        const leaf = Leaf.getLeaf(element);
+        if (leaf)
+            leafs.push(leaf);
+
+        Array.from(element.children).forEach(child=>{
+            leafs = traverseChildren(child, leafs);
+        })
+     
+        return leafs;
+    }
+    const leafs = [];
+    leafs = traverseChildren(doc.body, leafs);
+    return leafs
+}
+
+
+// ================================
+
+
+// calc proximity of Leafs based on html cusin or sibling or parent or child
+
+class Branch{
+    constructor(){
+        this.guide = ""
+        this.label = this.getLabel()
+        this.context = this.getContext()
+    }
+    getLabel(){
+    }
+    getContext(){
+    }
+}
+class BOptions extends Branch{
+    // LOptions>1 spatial
+}
+class BInput extends Branch{
+    // LInput and spatial
+}
+class BSelect extends Branch{
+    // LSelect and spatial
+    // LOptions>1 and non spatial
+    // Children
+}
+class BDropdown extends Branch{
+    // LText is spatial
+    // LOptions>1 and non spatial
+    // Cusin score
+}
+class BSubmit extends Branch{
+    // LText and pointer and spatial
+}
+
+function getBranches(leafs){
+    leafs.forEach(leaf=>{
+        
+    })
+}
+
+
+
+const LeafClasses = [LOption, LText, LInput, LSelect, LImage]
+
+
+const zDoc = document
+const pageRect = getPageRect(zDoc)
+
+const minMediaSize = 3;
+const minImageSize = 5;
+const zunit = parseFloat(getComputedStyle(zDoc.documentElement).fontSize);
+
+const zLeafs = getLeafs(zDoc)
+const zBranches = getBranches(zLeafs)
